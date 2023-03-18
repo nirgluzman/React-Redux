cf4de4f80c2b7d89d123f26bb06443d277a4170d
nirgluzman <nir.gluzman@gmail.com>
2023-03-18 20:04:45 +0100

    add login action

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,20 @@
 import React from "react";
 
+import { useDispatch } from "react-redux";
+import { login } from "../features/user";
+
 const Login = () => {
+  const dispatch = useDispatch();
+
   return (
     <div>
-      <button>Login</button>
+      <button
+        onClick={() =>
+          dispatch(login({ name: "Nir", age: 49, email: "nir@gmail" }))
+        }
+      >
+        Login
+      </button>
     </div>
   );
 };
